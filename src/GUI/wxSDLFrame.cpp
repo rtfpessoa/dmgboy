79f98115a1321509aa94838ebe32b3566a623acb
Pablo <devnull@localhost>
2010-08-13 15:27:01 +0200

    [Linux] Actualizado el proyecto de Codeblocks y arreglada la compilacion en linux

--- src/GUI/wxSDLFrame.cpp
+++ src/GUI/wxSDLFrame.cpp
@@ -1,16 +1,16 @@
 /*
  This file is part of gbpablog.
- 
+
  gbpablog is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  gbpablog is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with gbpablog.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,6 +22,7 @@
 #include "pause.xpm"
 #include "stop.xpm"
 #include "gb16.xpm"
+#include <string>
 
 IMPLEMENT_CLASS(SDLFrame, wxFrame)
 
@@ -41,25 +42,25 @@ SDLFrame::SDLFrame()
 {
     // Create the SDLFrame
     Create(0, IDF_FRAME, wxT("gbpablog"), wxDefaultPosition,
-           wxDefaultSize, wxCAPTION | wxSYSTEM_MENU | 
+           wxDefaultSize, wxCAPTION | wxSYSTEM_MENU |
            wxMINIMIZE_BOX | wxCLOSE_BOX);
 
 	SetIcon(wxIcon(gb16_xpm));
-	
+
     createMenuBar();
 	createToolBar();
-	
+
     // create the SDLPanel
     panel = new SDLScreen(this);
-	
-	
+
+
     video = new Video(panel);
 	cpu = new CPU(video);
 
 	cartridge = NULL;
-	
+
 	emuState = NotStartedYet;
-	
+
 }
 
 SDLFrame::~SDLFrame()
@@ -71,73 +72,74 @@ void SDLFrame::createMenuBar()
 {
 	// create the main menubar
     mb = new wxMenuBar();
-    
+
     // create the file menu
     wxMenu *fileMenu = new wxMenu;
 	fileMenu->Append(wxID_OPEN, wxT("&Open"));
 	fileMenu->Append(wxID_EXIT, wxT("E&xit"));
-    
+
     // add the file menu to the menu bar
     mb->Append(fileMenu, wxT("&File"));
-	
+
 	// create the emulation menu
     wxMenu *emulationMenu = new wxMenu;
     emulationMenu->Append(ID_START, wxT("&Start"));
 	emulationMenu->Append(ID_PAUSE, wxT("&Pause"));
 	emulationMenu->Append(ID_STOP, wxT("S&top"));
-    
+
     // add the file menu to the menu bar
     mb->Append(emulationMenu, wxT("&Emulation"));
-    
+
     // create the help menu
     wxMenu *helpMenu = new wxMenu;
     helpMenu->Append(wxID_ABOUT, wxT("&About"));
-    
+
     // add the help menu to the menu bar
     mb->Append(helpMenu, wxT("&Help"));
-    
+
     // add the menu bar to the SDLFrame
     SetMenuBar(mb);
 }
 
 void SDLFrame::createToolBar()
 {
-	
+
 	toolBar = new wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_HORIZONTAL|wxNO_BORDER);
 	wxBitmap bmpOpen(open_xpm);
 	toolBar->AddTool(wxID_OPEN, bmpOpen, wxT("Open"));
-	
+
 	toolBar->AddSeparator();
-	
+
 	wxBitmap bmpPlay(play_xpm);
 	toolBar->AddTool(ID_START, bmpPlay, wxT("Start"));
 
 	wxBitmap bmpPause(pause_xpm);
 	toolBar->AddTool(ID_PAUSE, bmpPause, wxT("Pause"));
-	
+
 	wxBitmap bmpStop(stop_xpm);
 	toolBar->AddTool(ID_STOP, bmpStop, wxT("Stop"));
-	
+
 	toolBar->Realize();
 	SetToolBar(toolBar);
 }
 
 void SDLFrame::onFileOpen(wxCommandEvent &) {
-	wxFileDialog* OpenDialog = new wxFileDialog(this, _("Choose a gameboy rom to open"), wxEmptyString, wxEmptyString, 
+	wxFileDialog* OpenDialog = new wxFileDialog(this, _("Choose a gameboy rom to open"), wxEmptyString, wxEmptyString,
 												_("Gameboy roms (*.gb)|*.gb"),
 												wxFD_OPEN, wxDefaultPosition);
-	
+
 	// Creates a "open file" dialog
 	if (OpenDialog->ShowModal() == wxID_OK) // if the user click "Open" instead of "Cancel"
 	{
 		cpu->Reset();
 		if (cartridge)
 			delete cartridge;
-		cartridge = new Cartridge(OpenDialog->GetPath().c_str());
+
+		cartridge = new Cartridge(std::string(OpenDialog->GetPath().mb_str()));
 		cpu->LoadCartridge(cartridge);
 		emuState = Playing;
 	}
-	
+
 	// Clean up after ourselves
 	OpenDialog->Destroy();
 }
@@ -184,7 +186,7 @@ void SDLFrame::onPlayUpdate(wxUpdateUIEvent& event)
 	else {
 		event.Enable(true);
 	}
-	
+
 }
 
 void SDLFrame::onPauseUpdate(wxUpdateUIEvent& event)
@@ -195,7 +197,7 @@ void SDLFrame::onPauseUpdate(wxUpdateUIEvent& event)
 	else {
 		event.Enable(true);
 	}
-	
+
 }
 
 void SDLFrame::onStopUpdate(wxUpdateUIEvent& event)
@@ -220,29 +222,29 @@ void SDLFrame::onStopUpdate(wxUpdateUIEvent& event)
 void SDLFrame::onIdle(wxIdleEvent &event)
 {
 	long duration = 16;
-	
+
 	long lastDuration = swFrame.Time();
 	swFrame.Start();
-	
+
 	long delay = 0;
-	
+
 	if (lastDuration > duration)
 		delay = lastDuration - duration;
-	
+
     if (emuState == Playing)
 	{
 		swExecution.Start();
-		
+
 		cpu->UpdatePad();
-		
+
 		cpu->Run(70149);
-		
+
 		delay += swExecution.Time();
 	}
-	
+
 	if (delay > duration)
 		delay = duration;
-	
+
 	event.RequestMore(true);
     wxMilliSleep(duration-delay);
 }
